61bf502e0e2ff422bafbbc562566c0b0bbb25e2b
Thomas Sibley <tsibley@cpan.org>
2014-07-24 22:18:03 +0000

    Import of TSIBLEY/Test-Deep-DateTime-RFC3339-0.03 from CPAN.
    gitpan-cpan-distribution: Test-Deep-DateTime-RFC3339
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         TSIBLEY/Test-Deep-DateTime-RFC3339-0.03.tar.gz
    gitpan-cpan-author:       TSIBLEY
    gitpan-cpan-maturity:     released

--- lib/Test/Deep/DateTime/RFC3339.pm
+++ lib/Test/Deep/DateTime/RFC3339.pm
@@ -3,7 +3,7 @@ package Test::Deep::DateTime::RFC3339;
 use strict;
 use warnings;
 use 5.008_005;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use Test::Deep::Cmp;    # isa
 
@@ -128,8 +128,8 @@ Test::Deep::DateTime::RFC3339 - Test RFC3339 timestamps are within a certain tol
 
 =head1 DESCRIPTION
 
-Test::Deep::DateTime::RFC3339 provides a single function, L<<
-C<datetime_rfc3339> | /datetime_rfc3339 >>, which is used with L<Test::Deep> to
+Test::Deep::DateTime::RFC3339 provides a single function,
+L<< C<datetime_rfc3339>|/datetime_rfc3339 >>, which is used with L<Test::Deep> to
 check that the B<string> value gotten is an RFC3339-compliant timestamp.  It
 can also check if the timestamp is equal to, or within optional tolerances of,
 an expected timestamp.
